40371334863cd437a55883a5922a9c99c0c694ef
As90909w <163041654+As90909w@users.noreply.github.com>
2024-03-14 12:09:03 -0700

    Create power.swift

---
+++ power.swift
@@ -0,0 +1,22 @@
+<script>
+var VERSION=2.052;
+var BETA=0;
+var App=typeof App==='undefined'?0:App;
+</script>
+
+<link href="style.css?v=9" rel="stylesheet" type="text/css">
+<script  src="main.js?v=10"></script>
+
+<!-- ad -->
+<script src="showads.js"></script><!-- this just detects adblockers so we can adjust the layout and play nice -->
+<!-- /ad -->
+
+<!--[if IE]>
+<style type="text/css">
+	#ifIE9{display:block;}
+</style>
+<![endif]-->
+
+<!--[if lt IE9]><script src="excanvas.compiled.js"></script><![endif]-->
+</head>
+<body>
